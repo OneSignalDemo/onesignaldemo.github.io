41bb7da2530ec6d3df3291b4223ae4007f70e89f
JonF <jononesignal@MacBook-Pro-2.local>
2021-07-23 10:19:25 -0700

    small updates

--- OneSignalMethods.js
+++ OneSignalMethods.js
@@ -10,13 +10,13 @@
         //appId: "380dc082-5231-4cc2-ab51-a03da5a0e4c2", // testing
         allowLocalhostAsSecureOrigin: true,
         appId: "5e605fcd-de88-4b0a-a5eb-5c18b84d52f3", //main
-        promptOptions:{
-          slidedown:{
+        promptOptions: {
+          slidedown: {
             autoPrompt: true,
             enabled: true,
-            actionMessage:"Get Updated on our products?",
-            acceptButtonText:"Of course!",
-            cancelButtonText:"Maybe later",
+            actionMessage: "Get Updated on our products?",
+            acceptButtonText: "Of course!",
+            cancelButtonText: "Maybe later",
             categories: {
               tags: [
                 {
@@ -37,23 +37,23 @@
         },
       });
     });
-    
-//------------------TEST CODE
-window.OneSignal.push(async () => {
-  window.OneSignal.addListenerForNotificationOpened(notification => {
-      console.log("notification: ", notification);
-      let timestamp = Math.floor(Date.now() / 1000);
-      OneSignal.sendTag("last_notification_click", timestamp);
-  });
-});
-//------------------TEST CODE
 
-    
+    //------------------TEST CODE
+    window.OneSignal.push(async () => {
+      window.OneSignal.addListenerForNotificationOpened(notification => {
+        console.log("notification: ", notification);
+        let timestamp = Math.floor(Date.now() / 1000);
+        OneSignal.sendTag("last_notification_click", timestamp);
+      });
+    });
+    //------------------TEST CODE
+
+
   };
   script.src = "https://cdn.onesignal.com/sdks/OneSignalSDK.js";
   d.getElementsByTagName("head")[0].appendChild(script);
 
-//TEST CODE
+  //TEST CODE
   // var osTagsForPage = {"kia": 0, "used": 0, "rio": 0}
 
   // function osUpdateTags(osTags) {
@@ -61,10 +61,10 @@ window.OneSignal.push(async () => {
   //     OneSignal.isPushNotificationsEnabled(function(isEnabled) {
   //       console.log("isEnabled: ", isEnabled);
   //       if (isEnabled) {        
-	// 	      OneSignal.sendTags(osTags).then(function(tagsSent) {
-	// 	      // Callback called when tags have finished sending
-	// 	      console.log("tagsSent: ", tagsSent)  
-	// 	      });  
+  // 	      OneSignal.sendTags(osTags).then(function(tagsSent) {
+  // 	      // Callback called when tags have finished sending
+  // 	      console.log("tagsSent: ", tagsSent)  
+  // 	      });  
   //       }
   //     });
   //   });
@@ -101,15 +101,25 @@ window.OneSignal.push(async () => {
   //     : [];
   // }
 
-// END TEST CODE
+  // END TEST CODE
 
 })(document);
 
-(function(c,a){if(!a.__SV){var b=window;try{var d,m,j,k=b.location,f=k.hash;d=function(a,b){return(m=a.match(RegExp(b+"=([^&]*)")))?m[1]:null};f&&d(f,"state")&&(j=JSON.parse(decodeURIComponent(d(f,"state"))),"mpeditor"===j.action&&(b.sessionStorage.setItem("_mpcehash",f),history.replaceState(j.desiredHash||"",c.title,k.pathname+k.search)))}catch(n){}var l,h;window.mixpanel=a;a._i=[];a.init=function(b,d,g){function c(b,i){var a=i.split(".");2==a.length&&(b=b[a[0]],i=a[1]);b[i]=function(){b.push([i].concat(Array.prototype.slice.call(arguments,
-  0)))}}var e=a;"undefined"!==typeof g?e=a[g]=[]:g="mixpanel";e.people=e.people||[];e.toString=function(b){var a="mixpanel";"mixpanel"!==g&&(a+="."+g);b||(a+=" (stub)");return a};e.people.toString=function(){return e.toString(1)+".people (stub)"};l="disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking has_opted_out_tracking clear_opt_in_out_tracking start_batch_senders people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user people.remove".split(" ");
-  for(h=0;h<l.length;h++)c(e,l[h]);var f="set set_once union unset remove delete".split(" ");e.get_group=function(){function a(c){b[c]=function(){call2_args=arguments;call2=[c].concat(Array.prototype.slice.call(call2_args,0));e.push([d,call2])}}for(var b={},d=["get_group"].concat(Array.prototype.slice.call(arguments,0)),c=0;c<f.length;c++)a(f[c]);return b};a._i.push([b,d,g])};a.__SV=1.2;b=c.createElement("script");b.type="text/javascript";b.async=!0;b.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?
-  MIXPANEL_CUSTOM_LIB_URL:"file:"===c.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";d=c.getElementsByTagName("script")[0];d.parentNode.insertBefore(b,d)}})(document,window.mixpanel||[]);
-  mixpanel.init("d810d40cdbc7dead2ff901838c696ccb", {batch_requests: true})
+(function (c, a) {
+  if (!a.__SV) {
+    var b = window; try { var d, m, j, k = b.location, f = k.hash; d = function (a, b) { return (m = a.match(RegExp(b + "=([^&]*)"))) ? m[1] : null }; f && d(f, "state") && (j = JSON.parse(decodeURIComponent(d(f, "state"))), "mpeditor" === j.action && (b.sessionStorage.setItem("_mpcehash", f), history.replaceState(j.desiredHash || "", c.title, k.pathname + k.search))) } catch (n) { } var l, h; window.mixpanel = a; a._i = []; a.init = function (b, d, g) {
+      function c(b, i) {
+        var a = i.split("."); 2 == a.length && (b = b[a[0]], i = a[1]); b[i] = function () {
+          b.push([i].concat(Array.prototype.slice.call(arguments,
+            0)))
+        }
+      } var e = a; "undefined" !== typeof g ? e = a[g] = [] : g = "mixpanel"; e.people = e.people || []; e.toString = function (b) { var a = "mixpanel"; "mixpanel" !== g && (a += "." + g); b || (a += " (stub)"); return a }; e.people.toString = function () { return e.toString(1) + ".people (stub)" }; l = "disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking has_opted_out_tracking clear_opt_in_out_tracking start_batch_senders people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user people.remove".split(" ");
+      for (h = 0; h < l.length; h++)c(e, l[h]); var f = "set set_once union unset remove delete".split(" "); e.get_group = function () { function a(c) { b[c] = function () { call2_args = arguments; call2 = [c].concat(Array.prototype.slice.call(call2_args, 0)); e.push([d, call2]) } } for (var b = {}, d = ["get_group"].concat(Array.prototype.slice.call(arguments, 0)), c = 0; c < f.length; c++)a(f[c]); return b }; a._i.push([b, d, g])
+    }; a.__SV = 1.2; b = c.createElement("script"); b.type = "text/javascript"; b.async = !0; b.src = "undefined" !== typeof MIXPANEL_CUSTOM_LIB_URL ?
+      MIXPANEL_CUSTOM_LIB_URL : "file:" === c.location.protocol && "//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//) ? "https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js" : "//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js"; d = c.getElementsByTagName("script")[0]; d.parentNode.insertBefore(b, d)
+  }
+})(document, window.mixpanel || []);
+mixpanel.init("d810d40cdbc7dead2ff901838c696ccb", { batch_requests: true })
 
 window.addEventListener("load", () => {
 
@@ -132,29 +142,29 @@ window.addEventListener("load", () => {
 
 
   // -------------------------------- OneSignal Examples -------------------------------- //
-  
+
   OneSignal.push(function () {
     var isPushSupported = OneSignal.isPushNotificationsSupported();
     console.log("Push Supported on Browser: ", isPushSupported);
-    OneSignal.isPushNotificationsEnabled(function(isEnabled) {
+    OneSignal.isPushNotificationsEnabled(function (isEnabled) {
       if (isEnabled) {
         console.log("Push notifications are enabled!");
         OneSignal.getUserId(function (userId) {
           console.log("OneSignal Push User ID:", userId);
         });
-        OneSignal.getEmailId(function(emailId) {
+        OneSignal.getEmailId(function (emailId) {
           console.log("OneSignal Email User ID:", emailId)
         })
         async function getExternalUserId() {
           var externalUserId = await OneSignal.getExternalUserId();
-          console.log("OneSignal External User ID set:", externalUserId );
+          console.log("OneSignal External User ID set:", externalUserId);
           console.log("Settings External User ID to Mixpanel");
           mixpanel.people.set("$onesignal_user_id", externalUserId);
         }
         getExternalUserId();
       }
       else
-        console.log("Push notifications are not enabled yet.");    
+        console.log("Push notifications are not enabled yet.");
     });
 
     // SUBSCRIPTION CHANGE EVENT
@@ -162,26 +172,26 @@ window.addEventListener("load", () => {
       console.log("The user's subscription state is now:", isSubscribed);
       mixpanel.track(
         "Push Subscription Changed",
-        {"isSubscribed": isSubscribed}
+        { "isSubscribed": isSubscribed }
       );
       OneSignal.getUserId(function (userId) {
         console.log("OneSignal User ID:", userId);
       });
-      
-      OneSignal.setExternalUserId(externalUserId).then(function() {
+
+      OneSignal.setExternalUserId(externalUserId).then(function () {
         console.log("externalUserId set after subscription change: ", externalUserId);
         console.log("Settings External User ID to Mixpanel");
         mixpanel.people.set("$onesignal_user_id", externalUserId);
       })
-      
+
     });
 
-    OneSignal.on('notificationPermissionChange', function(permissionChange) {
+    OneSignal.on('notificationPermissionChange', function (permissionChange) {
       var currentPermission = permissionChange.to;
       console.log('New permission state:', currentPermission);
       mixpanel.track(
         "Notification Permission Changed",
-        {"currentPermission": currentPermission}
+        { "currentPermission": currentPermission }
       );
     });
   });
@@ -192,22 +202,24 @@ window.addEventListener("load", () => {
     updateEmailWithFieldsButton != null
   ) {
     updateEmailWithFieldsButton.addEventListener("click", () => {
-      OneSignal.push(function() {
+      OneSignal.push(function () {
         let email = document.getElementById("email_field").value
         console.log("about to setEmail: ", email)
         //mixpanel.alias(externalUserId + "_email", externalUserId);
         //mixpanel.identify(externalUserId + "_email");
-        OneSignal.setEmail(email)          
-          .then(function(emailId) {
-            // Callback called when email have finished sending
-            console.log("emailId: ", emailId);
-            OneSignal.setExternalUserId(externalUserId).then(function() {
-              console.log("externalUserId set after email provided: ", externalUserId);
-            })
-            mixpanel.people.set({
-              $email: email//,$onesignal_user_id: emailId
+        OneSignal.logoutEmail().then(function () {
+          OneSignal.setEmail(email)
+            .then(function (emailId) {
+              // Callback called when email have finished sending
+              console.log("emailId: ", emailId);
+              OneSignal.setExternalUserId(externalUserId).then(function () {
+                console.log("externalUserId set after email provided: ", externalUserId);
+              })
+              mixpanel.people.set({
+                $email: email//,$onesignal_user_id: emailId
+              });
             });
-          }); 
+        })
       });
     })
   }
@@ -218,12 +230,12 @@ window.addEventListener("load", () => {
     tagUserWithFieldsButton != null
   ) {
     tagUserWithFieldsButton.addEventListener("click", () => {
-      OneSignal.push(function() {            
+      OneSignal.push(function () {
         OneSignal.sendTag(document.getElementById("tagKey").value, document.getElementById("tagValue").value)
-          .then(function(tagsSent) {
+          .then(function (tagsSent) {
             // Callback called when tags have finished sending
-            console.log("tagsSent: ", tagsSent);  
-          }); 
+            console.log("tagsSent: ", tagsSent);
+          });
       });
     })
   }
@@ -240,7 +252,7 @@ window.addEventListener("load", () => {
       });
       console.log("Mixpanel $first_name & $last_name updated: ", document.getElementById("$first_name").value + " " + document.getElementById("$last_name").value);
     })
-  }  
+  }
 
   const showCategorySlidePrompt = document.getElementById("showCategorySlidePrompt");
   if (
@@ -248,12 +260,12 @@ window.addEventListener("load", () => {
     showCategorySlidePrompt != null
   ) {
     showCategorySlidePrompt.addEventListener("click", () => {
-      OneSignal.push(function() {            
+      OneSignal.push(function () {
         OneSignal.showCategorySlidedown();
       });
     })
   }
-  
+
 
 
   // -------------------------------- Abandoned Cart Example -------------------------------- //
@@ -399,7 +411,7 @@ function updateOSOnCartPurchase(checkoutPriceTotal, checkoutItemsTotal) {
   let purchasedItemCount = parseInt(checkoutItemsTotal);
 
   OneSignal.push(function () {
-    OneSignal.getTags(function(tags) {
+    OneSignal.getTags(function (tags) {
       var purchase_amount = 0;
       if (tags.purchase_amount) {
         purchase_amount = parseInt(tags.purchase_amount)
