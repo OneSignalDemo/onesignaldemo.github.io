e5378a46ddc539488db12445b7f32d6847639f40
JonF <jononesignal@MacBook-Pro-2.local>
2021-09-25 18:08:10 -0700

    fix

--- OneSignalMethods.js
+++ OneSignalMethods.js
@@ -229,25 +229,18 @@ window.addEventListener("load", () => {
   }
 
   // -------------------------------- Google SignIn Example -------------------------------- //
-  const googleSignInButton = document.querySelector(".g-signin2");
-  if (
-    typeof googleSignInButton != "undefined" &&
-    googleSignInButton != null
-  ) {
-    googleSignInButton.addEventListener("click", (googleUser) => {
-      // Useful data for your client-side scripts:
-      var profile = googleUser.getBasicProfile();
-      console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-      console.log('Full Name: ' + profile.getName());
-      console.log('Given Name: ' + profile.getGivenName());
-      console.log('Family Name: ' + profile.getFamilyName());
-      console.log("Image URL: " + profile.getImageUrl());
-      console.log("Email: " + profile.getEmail());
+  function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
 
-      // The ID token you need to pass to your backend:
-      var id_token = googleUser.getAuthResponse().id_token;
-      console.log("ID Token: " + id_token);
-    });
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
   }
 
   const googleLogoutButton = document.getElementById("logOutRemoveExternalUserId");
