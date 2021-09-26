b91ec191e8036b04a87fb9c0c2072d307cd1a6e8
JonF <jononesignal@MacBook-Pro-2.local>
2021-09-25 19:31:58 -0700

    fix

--- OneSignalMethods.js
+++ OneSignalMethods.js
@@ -224,6 +224,7 @@ window.addEventListener("load", () => {
     googleLogoutButton.addEventListener("click", () => {
       var auth2 = gapi.auth2.getAuthInstance();
       auth2.signOut().then(function () {
+        OneSignal.removeExternalUserId();
         console.log('User signed out.');
       });
     });
@@ -396,4 +397,21 @@ function updateOSOnCartPurchase(checkoutPriceTotal, checkoutItemsTotal) {
       console.log("Purchased Item Count ", purchasedItemCount);
     })
   });
+}
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId());
+  let googleUserId = profile.get();
+  osSetExternalUserId(googleUserId);
+
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());
+
+  // The ID token you need to pass to your backend:
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
 }
