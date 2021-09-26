df31180487220fd18f210517612adadc9f69b7cb
JonF <jononesignal@MacBook-Pro-2.local>
2021-09-25 20:05:22 -0700

    fix

--- OneSignalMethods.js
+++ OneSignalMethods.js
@@ -353,28 +353,26 @@ function updateOSOnCartPurchase(checkoutPriceTotal, checkoutItemsTotal) {
 
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId());
   let googleUserId = profile.getId();
+  console.log("googleUserId: ", googleUserId);
   osSetExternalUserId(googleUserId);
-
+  let email = profile.getEmail();
+  console.log("email: ", email);
+  osSetEmail(email);
   console.log('Full Name: ' + profile.getName());
   console.log('Given Name: ' + profile.getGivenName());
   console.log('Family Name: ' + profile.getFamilyName());
   console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
-
   // The ID token you need to pass to your backend:
   //var id_token = googleUser.getAuthResponse().id_token;
   //console.log("ID Token: " + id_token);
 }
 
 function onSignOut() {
-  googleLogoutButton.addEventListener("click", () => {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      OneSignal.removeExternalUserId();
-      console.log('User signed out.');
-    });
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    OneSignal.removeExternalUserId();
+    console.log('User signed out.');
   });
 }
 
